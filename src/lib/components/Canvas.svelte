0d6645b9de5365db34ee906dbb881cc4d6e03ebf
theandrew168 <andrew@shallowbrooksoftware.com>
2024-01-05 19:20:05 -0600

    Render some hex tiles!

--- src/lib/components/Canvas.svelte
+++ src/lib/components/Canvas.svelte
@@ -13,8 +13,9 @@
 	onMount(async () => {
 		gl = initGL(canvas);
 
-		const spritesheet = await loadImage("/img/countryside.png", true);
+		const spritesheet = await loadImage("/img/hex.png", true);
 		renderer = new Renderer2D(canvas, gl, spritesheet);
+		renderer.flush();
 
 		frame = requestAnimationFrame(draw);
 	});
@@ -26,26 +27,28 @@
 	});
 
 	function draw(time: DOMHighResTimeStamp) {
-		const countryside = new Sprite(64, 64);
-		const grass = countryside.slice(32, 32, 0, 0);
-		const flower = countryside.slice(32, 32, 0, 1);
-		const boxFront = countryside.slice(32, 32, 1, 0);
-		const boxTop = countryside.slice(32, 32, 1, 1);
+		const hex = new Sprite(64, 64);
+		const grass = hex.slice(16, 16, 0, 0);
+		const sand = hex.slice(16, 16, 1, 0);
+		const water = hex.slice(16, 16, 2, 0);
+		const bug = hex.slice(16, 16, 3, 0);
 
-		renderer.draw(countryside, { x: 0, y: 0, r: time / 10 });
-		renderer.draw(countryside, { x: 64, y: 0 });
-		renderer.draw(countryside, { x: -64, y: 0 });
-		renderer.draw(countryside, { x: 0, y: 64 });
-		renderer.draw(countryside, { x: 0, y: -64 });
-		renderer.draw(countryside, { x: 128, y: 128, sx: 3, sy: 3 });
-		renderer.draw(countryside, { x: -96, y: -96, sx: 2, sy: 2 });
-		renderer.draw(countryside, { x: 0, y: -192, sx: Math.abs(Math.sin(time / 1000)) * 5 });
-		renderer.draw(countryside, { x: Math.sin(time / 500) * 256, y: Math.cos(time / 500) * 256 });
-		renderer.draw(countryside, { x: 192, y: 0, r: time / 10 });
-		renderer.draw(grass, { x: -128, y: 0, sx: 2, sy: 2 });
-		renderer.draw(flower, { x: -192, y: 0, sx: 2, sy: 2 });
-		renderer.draw(boxFront, { x: -256, y: 0, sx: 2, sy: 2 });
-		renderer.draw(boxTop, { x: -256, y: 64, sx: 2, sy: 2 });
+		// top row
+		renderer.draw(grass, { x: 0 * 16 * 4 - 32, y: 16 * 4 - 12, sx: 4, sy: 4 });
+		renderer.draw(sand, { x: 1 * 16 * 4 - 32, y: 16 * 4 - 12, sx: 4, sy: 4 });
+
+		// middle row
+		renderer.draw(water, { x: -1 * 16 * 4, y: 0, sx: 4, sy: 4 });
+		renderer.draw(grass, { x: 0 * 16 * 4, y: 0, sx: 4, sy: 4 });
+		renderer.draw(sand, { x: 1 * 16 * 4, y: 0, sx: 4, sy: 4 });
+
+		// bottom row
+		renderer.draw(grass, { x: 0 * 16 * 4 - 32, y: -16 * 4 + 12, sx: 4, sy: 4 });
+		renderer.draw(sand, { x: 1 * 16 * 4 - 32, y: -16 * 4 + 12, sx: 4, sy: 4 });
+
+		// bug
+		renderer.draw(bug, { x: 0 * 16 * 4, y: 0, sx: 2, sy: 2 });
+
 		renderer.flush();
 
 		frame = requestAnimationFrame(draw);
