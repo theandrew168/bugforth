9203b188465eec59438d0f0982f02935d7898799
theandrew168 <andrew@shallowbrooksoftware.com>
2026-07-18 10:47:17 -0500

    Update packages, migrate to Svelte 5

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
+import { mount } from "svelte";
+
 import App from "./App.svelte";
 
 // Lookup the element with ID "app" in the project's
 // index.html and attach the Svelte App to it.
 const target = document.getElementById("app")!;
-const app = new App({ target });
+const app = mount(App, { target });
 
 export default app;
